a1150ee9ba705425e87c2eb9b625964f09624053
Roshan Patel <roshanpatel12309@gmail.com>
2025-07-17 19:25:33 +0545

    Added register user features

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,24 @@
  const express = require('express');
  const router = express.Router();
+ const userModel = require('../models/userModel')
 
 router.get('/', (req,res) => {
     res.send('hello man');
-})
+});
+
+router.post('/register',async (req,res) => {
+   try{
+     let { email , fullname, password } = req.body;
+    let registeredUser =await userModel.create({
+        email,
+        fullname,
+        password,
+    })
+    console.log(registeredUser);
+    res.status(202).send("Created users");
+   } catch(err){
+    res.send(err.message);
+   }
+});
 
  module.exports = router;
